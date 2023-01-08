1515fda09244d10e93a68268b3cfdb52de5e5150
nazrul0 <nazrulsyahmi00@gmail.com>
2023-01-09 00:28:49 +0800

    ex 1.11: unicafe step6

--- 1.6/src/Statistics.js
+++ 1.6/src/Statistics.js
@@ -17,12 +17,39 @@ const Statistics = (props) => {
   return (
     <div>
       <h1>statistics</h1>
-      <StatisticLine text="good" statistic={props.good} />
+      {/* <StatisticLine text="good" statistic={props.good} />
       <StatisticLine text="neutral" statistic={props.neutral} />
       <StatisticLine text="bad" statistic={props.bad} />
       <StatisticLine text="all" statistic={total} />
       <StatisticLine text="average" statistic={average} />
-      <StatisticLine text="positive" statistic={positive + "%"} />
+      <StatisticLine text="positive" statistic={positive + "%"} /> */}
+
+      <table>
+        <tr>
+          <td>good</td>
+          <td>{props.good}</td>
+        </tr>
+        <tr>
+          <td>neutral</td>
+          <td>{props.neutral}</td>
+        </tr>
+        <tr>
+          <td>bad</td>
+          <td>{props.bad}</td>
+        </tr>
+        <tr>
+          <td>all</td>
+          <td>{total}</td>
+        </tr>
+        <tr>
+          <td>average</td>
+          <td>{average}</td>
+        </tr>
+        <tr>
+          <td>positive</td>
+          <td>{positive + "%"}</td>
+        </tr>
+      </table>
     </div>
   );
 };
